9b302673f56913caf2ef672385d65d892371c9e4
wuchang <kkang0820qus@gmail.com>
2023-05-28 12:09:18 +0900

    프로젝트 생성

---
+++ src/main/java/kr/co/moneybridge/FinalProjectBeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.moneybridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectBeApplication.class, args);
+	}
+
+}
