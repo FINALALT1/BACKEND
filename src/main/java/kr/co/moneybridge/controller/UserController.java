c9f4a9791ae0d1ec332f1b1fdfa86ed7f88129c4
wuchang <kkang0820qus@gmail.com>
2023-05-28 15:29:46 +0900

    초기 세팅

---
+++ src/main/java/kr/co/moneybridge/controller/UserController.java
@@ -0,0 +1,53 @@
+package kr.co.moneybridge.controller;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import kr.co.moneybridge.core.annotation.MyErrorLog;
+import kr.co.moneybridge.core.annotation.MyLog;
+import kr.co.moneybridge.core.auth.jwt.MyJwtProvider;
+import kr.co.moneybridge.core.auth.session.MyUserDetails;
+import kr.co.moneybridge.core.exception.Exception403;
+import kr.co.moneybridge.dto.ResponseDTO;
+import kr.co.moneybridge.dto.user.UserRequest;
+import kr.co.moneybridge.dto.user.UserResponse;
+import kr.co.moneybridge.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @MyErrorLog
+    @MyLog
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody @Valid UserRequest.JoinInDTO joinInDTO, Errors errors) {
+        UserResponse.JoinOutDTO joinOutDTO = userService.회원가입(joinInDTO);
+        ResponseDTO<?> responseDTO = new ResponseDTO<>(joinOutDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserRequest.LoginInDTO loginInDTO){
+        String jwt = userService.로그인(loginInDTO);
+        ResponseDTO<?> responseDTO = new ResponseDTO<>();
+        return ResponseEntity.ok().header(MyJwtProvider.HEADER, jwt).body(responseDTO);
+    }
+
+    @GetMapping("/s/user/{id}")
+    public ResponseEntity<?> detail(@PathVariable Long id, @AuthenticationPrincipal MyUserDetails myUserDetails) throws JsonProcessingException {
+        if(id.longValue() != myUserDetails.getUser().getId()){
+            throw new Exception403("권한이 없습니다");
+        }
+        UserResponse.DetailOutDTO detailOutDTO = userService.회원상세보기(id);
+        //System.out.println(new ObjectMapper().writeValueAsString(detailOutDTO));
+        ResponseDTO<?> responseDTO = new ResponseDTO<>(detailOutDTO);
+        return ResponseEntity.ok(responseDTO);
+    }
+}
