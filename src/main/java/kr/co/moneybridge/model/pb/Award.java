6a6e9e87da55c2c6f3e387d51a23fed94213c2de
wuchang <kkang0820qus@gmail.com>
2023-06-05 15:20:07 +0900

    env: model 수정

---
+++ src/main/java/kr/co/moneybridge/model/pb/Award.java
@@ -0,0 +1,42 @@
+package kr.co.moneybridge.model.pb;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "Award_tb")
+@Entity
+public class Award {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PB pb;
+
+    @Column(nullable = false, length = 90)
+    private String content; // 수상내역
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean status;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
