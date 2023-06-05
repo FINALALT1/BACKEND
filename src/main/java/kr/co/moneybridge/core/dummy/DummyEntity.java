8ddd7a429376fad4b68be7640503c70f74b42f84
wuchang <kkang0820qus@gmail.com>
2023-06-05 17:06:07 +0900

    dummy entity

--- src/main/java/kr/co/moneybridge/core/dummy/DummyEntity.java
+++ src/main/java/kr/co/moneybridge/core/dummy/DummyEntity.java
@@ -1,6 +1,10 @@
 package kr.co.moneybridge.core.dummy;
 
+import kr.co.moneybridge.model.board.Board;
+import kr.co.moneybridge.model.pb.*;
 import kr.co.moneybridge.model.user.User;
+import kr.co.moneybridge.model.user.UserPropensity;
+import kr.co.moneybridge.model.user.UserRole;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDateTime;
@@ -9,10 +13,83 @@ public class DummyEntity {
     public User newUser(String username){
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return User.builder()
-                .username(username)
-                .password(passwordEncoder.encode("1234"))
+                .name(username)
+                .password(passwordEncoder.encode("password1234"))
                 .email(username+"@nate.com")
-                .role("USER")
+                .phoneNumber("01012345678")
+                .role(UserRole.USER)
+                .status(true)
+                .build();
+    }
+    public User newUserWithPropensity(String username){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return User.builder()
+                .name(username)
+                .password(passwordEncoder.encode("password1234"))
+                .email(username+"@nate.com")
+                .phoneNumber("01012345678")
+                .propensity(UserPropensity.AGGRESSIVE)
+                .role(UserRole.USER)
+                .status(true)
+                .build();
+    }
+
+    public PB newPB(String username, Branch branch){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return PB.builder()
+                .name(username)
+                .password(passwordEncoder.encode("password1234"))
+                .email(username+"@nate.com")
+                .phoneNumber("01012345678")
+                .branch(branch)
+                .profile("profile.png")
+                .businessCard("card.png")
+                .career(10)
+                .speciality1(PBSpeciality.BOND)
+                .intro(username+" 입니다")
+                .msg("한줄메시지..")
+                .reservationInfo("10분 미리 도착해주세요")
+                .consultStart("09:00")
+                .consultEnd("18:00")
+                .consultNotice("월요일 불가능합니다")
+                .role(PBRole.PB)
+                .status(PBStatus.ACTIVE)
+                .build();
+    }
+
+    public PB newPBWithSpeciality(String username, Branch branch){
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return PB.builder()
+                .name(username)
+                .password(passwordEncoder.encode("password1234"))
+                .email(username+"@nate.com")
+                .phoneNumber("01012345678")
+                .branch(branch)
+                .profile("profile.png")
+                .businessCard("card.png")
+                .career(10)
+                .speciality1(PBSpeciality.BOND)
+                .speciality2(PBSpeciality.ETF)
+                .intro(username+" 입니다")
+                .msg("한줄메시지..")
+                .reservationInfo("10분 미리 도착해주세요")
+                .consultStart("09:00")
+                .consultEnd("18:00")
+                .consultNotice("월요일 불가능합니다")
+                .role(PBRole.PB)
+                .status(PBStatus.ACTIVE)
+                .build();
+    }
+
+    public Board newBoard(String title, PB pb){
+        return Board.builder()
+                .pb(pb)
+                .title(title)
+                .thumbnail("thumbnail.png")
+                .content("content 입니다")
+                .tag1()
+                .phoneNumber("01012345678")
+                .role(UserRole.USER)
                 .status(true)
                 .build();
     }
@@ -21,10 +98,12 @@ public class DummyEntity {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         return User.builder()
                 .id(id)
-                .username(username)
-                .password(passwordEncoder.encode("1234"))
+                .name(username)
+                .password(passwordEncoder.encode("password1234"))
                 .email(username+"@nate.com")
-                .role("USER")
+                .phoneNumber("01012345678")
+                .propensity(UserPropensity.AGGRESSIVE)
+                .role(UserRole.USER)
                 .status(true)
                 .createdAt(LocalDateTime.now())
                 .build();
