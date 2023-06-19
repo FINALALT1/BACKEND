83c9246e81ef9e7c076543ffbc8e968c91003941
wuchang <kkang0820qus@gmail.com>
2023-06-19 19:39:19 +0900

    Feat: 특정 PB의 컨텐츠 목록 가져오기

--- src/main/java/kr/co/moneybridge/dto/board/BoardResponse.java
+++ src/main/java/kr/co/moneybridge/dto/board/BoardResponse.java
@@ -38,6 +38,7 @@ public class BoardResponse {
         private String tag2;
         @ApiModelProperty(example = "안녕하세요", value = "한줄메세지")
         private String msg;
+        private Boolean isBookmark; //set해줘야함
 
         public BoardPageDTO(Board board, PB pb, Company company) {
             this.id = board.getId();
