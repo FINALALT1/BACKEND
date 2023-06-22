594f5158afe8be3f8a701a7a4c23f81f46618d11
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-06-22 19:35:16 +0900

    Feat: 예약 관련 스케쥴러 코드 수정

--- src/main/java/kr/co/moneybridge/core/scheduler/ReservationScheduler.java
+++ src/main/java/kr/co/moneybridge/core/scheduler/ReservationScheduler.java
@@ -3,24 +3,32 @@ package kr.co.moneybridge.core.scheduler;
 import kr.co.moneybridge.model.reservation.ReservationProcess;
 import kr.co.moneybridge.model.reservation.ReservationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class ReservationScheduler {
     private final ReservationRepository reservationRepository;
 
     // 상담일로부터 3일이 지난 예약은 상담완료 처리
-    // 1시간 주기로 실행
-    @Scheduled(fixedRate = 60 * 60 * 1000)
+    // 매일 0시에 실행
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
     public void updateReservationProcess() {
-        reservationRepository.findAllByTimeBeforeAndProcess(LocalDateTime.now().minusHours(72), ReservationProcess.CONFIRM)
-                .forEach(reservation -> {
-                    reservation.updateProcess(ReservationProcess.COMPLETE);
-                    reservationRepository.save(reservation);
-                });
+        try {
+            reservationRepository.findAllByTimeBeforeAndProcess(LocalDateTime.now().minusHours(72), ReservationProcess.CONFIRM)
+                    .forEach(reservation -> {
+                        reservation.updateProcess(ReservationProcess.COMPLETE);
+                        reservationRepository.save(reservation);
+                    });
+        } catch (Exception e) {
+            log.error("스케쥴러 작업 실패 : " + e.getMessage());
+        }
     }
 }
