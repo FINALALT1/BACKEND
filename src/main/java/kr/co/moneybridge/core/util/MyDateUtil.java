cc94b568e0559ff7f1dd93a3ac8d275f32f1e42e
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-06-26 16:06:50 +0900

    Fix: LocalDateTime 변수를 String으로 바꿀 때 AM이 오전, PM이 오후로 출력되도록 코드 수정

--- src/main/java/kr/co/moneybridge/core/util/MyDateUtil.java
+++ src/main/java/kr/co/moneybridge/core/util/MyDateUtil.java
@@ -16,7 +16,7 @@ public class MyDateUtil {
         if (localDateTime == null) {
             return null;
         }
-        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 mm분"));
+        return localDateTime.format(DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 mm분")).replace("AM", "오전").replace("PM", "오후");
     }
 
     public static LocalDateTime StringToLocalDateTime(String string) {
