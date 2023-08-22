55defab259c88583978b9f8e19459ae2196af516
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-08-22 13:34:09 +0900

    Feat: 스티비 주소록 구독 취소 메서드 추가

--- src/main/java/kr/co/moneybridge/core/util/StibeeUtil.java
+++ src/main/java/kr/co/moneybridge/core/util/StibeeUtil.java
@@ -17,9 +17,7 @@ import org.springframework.web.client.RestTemplate;
 public class StibeeUtil {
     // 주소록(전체)
     private final String TOTAL_LIST_URL = "https://api.stibee.com/v1/lists/269642/subscribers";
-    // 주소록(관리자)
-    private final String ADMIN_LIST_URL = "https://api.stibee.com/v1/lists/276462/subscribers";
-    // 주소록(투자자)
+    // 주소록(일반)
     private final String USER_LIST_URL = "https://api.stibee.com/v1/lists/272140/subscribers";
     // 주소록(PB)
     private final String PB_LIST_URL = "https://api.stibee.com/v1/lists/272138/subscribers";
@@ -32,15 +30,13 @@ public class StibeeUtil {
     }
 
     /**
-     * 스티비 주소록 구독
+     * 스티비 주소록 구독자 추가
      */
     public void subscribe(String role, String email, String name) {
-        // role에 따라 구독할 주소록 결정
+        // role에 따라 구독자를 추가할 주소록 결정
         String requestUrl = "";
         switch (role) {
             case "ADMIN":
-                requestUrl = ADMIN_LIST_URL;
-                break;
             case "USER":
                 requestUrl = USER_LIST_URL;
                 break;
@@ -48,9 +44,17 @@ public class StibeeUtil {
                 requestUrl = PB_LIST_URL;
                 break;
             default:
-                throw new Exception500("주소록 구독 실패 : 해당 사용자의 role이 존재하지 않습니다.");
+                throw new Exception500("구독자 추가 실패 : 해당 사용자의 role이 존재하지 않거나 잘못된 형태입니다.");
         }
 
+        // 역할에 해당하는 주소록에 구독자 추가
+        subscribeRequest(requestUrl, email, name);
+        // 전체 주소록에 구독자 추가
+        subscribeRequest(TOTAL_LIST_URL, email, name);
+    }
+
+    // 구독자 추가 API 요청
+    private void subscribeRequest(String requestUrl, String email, String name) {
         RestTemplate template = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
@@ -67,7 +71,7 @@ public class StibeeUtil {
         subscribers.put("name", name);
         subscribersArray.put(subscribers);
         requestBody.put("subscribers", subscribersArray); // 구독자 정보
-        // 광고성 정보 수신 동의와 관련된 헤더도 있으나 일단 생략
+        // 광고성 정보 수신 동의와 관련된 헤더도 있으나 여기선 생략
 
         HttpEntity<String> request = new HttpEntity<>(requestBody.toString(), headers);
 
@@ -98,7 +102,79 @@ public class StibeeUtil {
 
         if (root.path("Ok").asText().equals("false")) { // 실패
             JsonNode error = root.path("Error");
+            log.error("구독자 추가 요청 실패 : " + error.path("message").asText());
+        } else {
+            log.info("구독자 추가 요청 성공 : " + email);
+        }
+    }
+
+    /**
+     * 스티비 주소록 구독자 완전삭제
+     */
+    public void withdraw(String role, String email) {
+        // role에 따라 구독자를 삭제할 주소록 결정
+        String requestUrl = "";
+        switch (role) {
+            case "ADMIN":
+            case "USER":
+                requestUrl = USER_LIST_URL;
+                break;
+            case "PB":
+                requestUrl = PB_LIST_URL;
+                break;
+            default:
+                throw new Exception500("구독자 삭제 실패 : 해당 사용자의 role이 존재하지 않거나 잘못된 형태입니다.");
+        }
+
+        // 역할에 해당하는 주소록에서 구독자 삭제
+        withdrawRequest(requestUrl, email);
+        // 전체 주소록에서 구독자 삭제
+        withdrawRequest(TOTAL_LIST_URL, email);
+    }
+
+    // 구독자 완전 삭제 API 요청
+    private void withdrawRequest(String requestUrl, String email) {
+        RestTemplate template = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("AccessToken", API_KEY);
+
+        JSONArray requestBody = new JSONArray();
+        requestBody.put(email);
+
+        HttpEntity<String> request = new HttpEntity<>(requestBody.toString(), headers);
+
+        // API 요청
+        ResponseEntity<String> response = null;
+        try {
+            response = template.exchange(
+                    requestUrl,
+                    HttpMethod.DELETE,
+                    request,
+                    String.class
+            );
+        } catch (Exception e) {
+            log.error("API 요청 실패 : " + e.getMessage());
+            return;
+        }
+
+        String responseBody = response.getBody();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = null;
+        try {
+            root = mapper.readTree(responseBody);
+        } catch (Exception e) {
+            log.error("Json to JsonNode 실패 : " + e.getMessage());
+            return;
+        }
+
+        if (root.path("Ok").asText().equals("false")) { // 실패
+            JsonNode error = root.path("Error");
             log.error("주소록 구독 요청 실패 : " + error.path("message").asText());
+        } else {
+            log.info("주소록 구독 요청 성공 : " + email);
         }
     }
 }
